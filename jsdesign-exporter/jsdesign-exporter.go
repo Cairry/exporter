e9fe2574441b8952716fbe6423434a31685f4374
Cairry <115769353+Cairry@users.noreply.github.com>
2023-03-06 16:08:38 +0800

    [updage]: 优化代码

--- jsdesign-exporter/jsdesign-exporter.go
+++ jsdesign-exporter/jsdesign-exporter.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"crypto/tls"
 	"exporter/config"
 	"fmt"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"net/http"
+	"time"
 )
 
 var (
@@ -20,39 +22,54 @@ var cc = prometheus.NewGaugeVec(
 	prometheus.GaugeOpts{
 		Name: "url_interface_state_code",
 	},
-	[]string{"app"},
+	[]string{"app", "url"},
 )
 
 // Gauge metrics
-func Gauge(domainList map[string]string) {
-	// 注册指标
-	EmptyRegistry.MustRegister(cc)
+func Gauge(srvName, domainName string) {
 
-	for srvName, domainName := range domainList {
-
-		res, err := http.Get("http://" + domainName)
-		if err != nil {
-			fmt.Println("[ERROR]: 访问接口异常,", err)
-		} else {
-			StatusCode := res.StatusCode
-			if StatusCode == 200 {
-				UrlStateCode = 1
-			} else {
-				UrlStateCode = 0
-			}
-		}
-
-		// 写入标签
-		cc.With(prometheus.Labels{
-			"app": srvName,
-		}).Set(float64(UrlStateCode))
+	client := &http.Client{
+		// 设置请求超时时间
+		Timeout: 1 * time.Second,
+		// 跳过安全检查
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
 	}
+
+	_, err := client.Get(domainName)
+	if err != nil {
+		fmt.Printf("[ERROR %s]: 访问接口异常,%s\n", time.Now(), err)
+		UrlStateCode = 0
+	} else {
+		UrlStateCode = 1
+	}
+
+	// 写入标签
+	cc.With(prometheus.Labels{
+		"app": srvName,
+		"url": domainName,
+	}).Set(float64(UrlStateCode))
 
 }
 
-// RunServer 启动服务
 func RunServer() {
+
+	fmt.Printf("[INFO %s]: 服务已就绪!\n", time.Now())
+
+	// 注册指标
+	EmptyRegistry.MustRegister(cc)
+
 	http.HandleFunc("/metrics", func(writer http.ResponseWriter, request *http.Request) {
+
+		fmt.Printf("[INFO %s]: --- 开始抓取指标! ---\n", time.Now())
+		for srvName, domainName := range config.DomainMap {
+
+			// 探测 Domain 状态
+			go Gauge(srvName, domainName)
+
+		}
+
 		promhttp.HandlerFor(EmptyRegistry,
 			promhttp.HandlerOpts{ErrorHandling: promhttp.ContinueOnError}).ServeHTTP(writer, request)
 	})
@@ -67,9 +84,6 @@ func main() {
 
 	// 初始化配置文件
 	config.Config()
-
-	// 探测 Domain
-	go Gauge(config.DomainMap)
 
 	// 启动服务
 	RunServer()
